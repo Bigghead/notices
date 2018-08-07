14399ef15f9a14eef5224019fa4556f96048542d
Bigghead <shavenchew@yahoo.com>
2018-08-06 19:35:51 -0700

    add ref to notice to delete by button

--- src/containers/UI/api_notification.js
+++ src/containers/UI/api_notification.js
@@ -1,5 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component, Ref } from 'react';
 import { connect } from 'react-redux';
+
+import { RESET_NOTIFICATION } from '../../store/notifications'
 
 import './api-notification.css';
 
@@ -16,17 +18,33 @@ const successStyle = { 'backgroundColor': 'lightgreen' };
 const errorStyle   = { 'backgroundColor': '#cb2d3e' };
 
 
-class API_Notification extends Component {
+class Notification extends Component {
+
+    constructor(props) {
+        super(props);
+        // === Need to reset the notice from other page === //
+        this.props.dispatch( { type: RESET_NOTIFICATION } )
+    }
+
+    componentDidMount() {
+        this.noticeRef = React.createRef();
+    }
+
+    dismissNotice = () => {
+        this.noticeRef.current.className = '';
+    }
 
     renderNotice = ( success, error) => {
         const { mode } = this.props;
         return (
-            <div id="toast" 
+            <div ref={ this.noticeRef }
+                 id="toast" 
                  className={ success || error ? 'show' : '' } 
                  style={ success ? successStyle : errorStyle }>
                 <div id="img">{ success ? 'OK' : 'Error:' }</div>
                 <div id="desc">
                     { success ? `Successfully loaded ${this.props.mode}` : `Error loading ${mode}` }
+                    <button className="dismiss_button" onClick={ this.dismissNotice }>X</button>
                 </div>
             </div>
         )
@@ -34,11 +52,11 @@ class API_Notification extends Component {
 
 
     render() {
-        const { currentLetterCase, success, error, isLoading } = this.props;
+        const { success, error, isLoading } = this.props;
         return (
             <div>
-                { isLoading && 'Loading' }
-                { this.renderNotice( success, error )}
+                { isLoading && 'Loading ... ' }
+                { this.renderNotice( success, error ) }
             </div>
             
         );
@@ -47,4 +65,4 @@ class API_Notification extends Component {
 
 export default connect(
     mapStateToProps,
-)(API_Notification);
+)(Notification);
